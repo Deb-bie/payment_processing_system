3a29034c64d359a4aaf517ee59bfca1ba1135ee2
Deb-bie <asamoahjaeldeborah@gmail.com>
2025-09-18 22:31:06 -0500

    not found exception handler

--- user-service/src/main/java/com/example/exceptions/ExceptionController.java
+++ user-service/src/main/java/com/example/exceptions/ExceptionController.java
@@ -33,4 +33,15 @@ public class ExceptionController {
                         .build()
         );
     }
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<ResponseHandler<String>> notFoundExceptionHandler (NotFoundException notFoundException) {
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(ResponseHandler.<String>builder()
+                        .code(HttpStatus.NOT_FOUND.value())
+                        .status(HttpStatus.NOT_FOUND)
+                        .message(notFoundException.getMessage())
+                        .build());
+    }
 }
