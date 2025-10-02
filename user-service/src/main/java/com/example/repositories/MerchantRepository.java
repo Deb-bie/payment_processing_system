2c1aa8a260d6b8a8cfaeb5848fd27bf1d2ca0783
Deb-bie <asamoahjaeldeborah@gmail.com>
2025-10-01 23:02:31 -0500

    added repository annotation

--- user-service/src/main/java/com/example/repositories/MerchantRepository.java
+++ user-service/src/main/java/com/example/repositories/MerchantRepository.java
@@ -2,8 +2,10 @@ package com.example.repositories;
 
 import com.example.entities.Merchant;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface MerchantRepository extends CrudRepository<Merchant, UUID> {
 }
